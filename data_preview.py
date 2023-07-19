1c8fc9a7e3d2987361defcc5ec220630200c862f
audreych <audreychung2352002@gmail.com>
2023-07-19 23:58:55 +0800

    Add New Function to Store Name of Data File in csv

--- data_preview.py
+++ data_preview.py
@@ -2,18 +2,28 @@ import sys
 from helper_code import *
 from team_code import train_challenge_model
 import graphing as plotter
+import csv
+
+def write_csv(patient_ids, output_file):
+    output_string = '\n'.join([str(elem) for elem in patient_ids])
+    with open(output_file, 'w') as f:
+        f.write(output_string)
+    return
 
 if __name__ == '__main__':
     # Parse the arguments.
-    if not (len(sys.argv) == 3):
-        raise Exception('Include the data and graph folders as arguments, e.g., data_preview.py data model.')
+    if not (len(sys.argv) == 4):
+        raise Exception('Include the data and graph folders as arguments, e.g., data_preview.py data graph data.csv.')
 
     # Define the data and model foldes.
     data_folder = sys.argv[1]
     graph_folder = sys.argv[2]
+    output_file = sys.argv[3]
     print('Finding Data Folder...')
     patient_ids = find_data_folders(data_folder)
+    
     # Create a folder for the model if it does not already exist.
     os.makedirs(graph_folder, exist_ok=True)
+    write_csv(patient_ids, output_file)
     plotter.plot_data_graph(patient_ids, data_folder, graph_folder)
     print('Done.')
