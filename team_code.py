37ee6f2ef550a6a37cb6ae7ce43acb59762c0e4e
audreych <audreychung2352002@gmail.com>
2023-08-13 23:00:33 +0800

    Change Epoch to 5

--- team_code.py
+++ team_code.py
@@ -51,7 +51,7 @@ def train_challenge_model(data_folder, model_folder, verbose):
     
     # Training parameters
     batch_size = 32
-    epochs = 30
+    epochs = 5
     threshold = 48
     # Find data files.
     if verbose >= 1:
